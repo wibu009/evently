5438b99e6c371a2ef7c28f7caf7055477c95ec5b
wibu009 <kingofdespair69@gmail.com>
2025-06-30 00:34:02 +0700

    add event statistics

---
+++ src/Modules/Attendance/Evently.Modules.Attendance.Domain/Events/EventStatistics.cs
@@ -0,0 +1,38 @@
+﻿namespace Evently.Modules.Attendance.Domain.Events;
+
+public sealed class EventStatistics
+{
+    private EventStatistics() { }   
+    
+    public Guid EventId { get; private set; }
+    public string Title { get; private set; }
+    public string Description { get; private set; }
+    public string Location { get; private set; }
+    public DateTime StartAtUtc { get; private set; }
+    public DateTime? EndAtUtc { get; private set; }
+    public int TicketsSold { get; private set; }
+    public int AttendeesCheckedIn { get; private set; }
+    public List<string> DuplicateCheckInTickets { get; private set; }
+    public List<string> InvalidCheckInTickets { get; private set; }
+    
+    public static EventStatistics Create(
+        Guid id,
+        string title,
+        string description,
+        string location,
+        DateTime startsAtUtc,
+        DateTime? endsAtUtc)
+    {
+        var @event = new EventStatistics
+        {
+            EventId = id,
+            Title = title,
+            Description = description,
+            Location = location,
+            StartAtUtc = startsAtUtc,
+            EndAtUtc = endsAtUtc
+        };
+
+        return @event;
+    }
+}
---
+++ src/Modules/Attendance/Evently.Modules.Attendance.Presentation/EventStatistics/GetEventStatisticsEndpoint.cs
@@ -0,0 +1,32 @@
+﻿using Evently.Common.Domain;
+using Evently.Common.Presentation.Endpoints;
+using Evently.Common.Presentation.Results;
+using Evently.Modules.Attendance.Application.EventStatistics.GetEventStatistics;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace Evently.Modules.Attendance.Presentation.EventStatistics;
+
+internal sealed class GetEventStatisticsEndpoint : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("event-statistics/{id}", async (Guid id, ISender sender) =>
+            {
+                Result<EventStatisticsResponse> result = await sender.Send(new GetEventStatisticsQuery(id));
+
+                return result.Match(Results.Ok, ApiResults.Problem);
+            })
+            .RequireAuthorization(Permissions.GetEventStatistics)
+            .WithTags(Tags.EventStatistics)
+            .WithName("Get Event Statistics")
+            .Produces<EventStatisticsResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .ProducesProblem(StatusCodes.Status500InternalServerError)
+            .WithSummary("Gets statistics for an event")
+            .WithDescription("Retrieves statistics for an event.");
+    }
+}
