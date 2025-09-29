c6c4842a64d6075b95061068b5770be22fb0f047
wibu009 <kingofdespair69@gmail.com>
2025-09-30 00:52:18 +0700

    extract ticketing module

---
+++ src/API/Evently.Ticketing.Api/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Evently.Ticketing.Api.Extensions;
+
+internal static class ConfigurationExtensions
+{
+    internal static void AddModuleConfiguration(this IConfigurationBuilder configurationBuilder, params string[] modules)
+    {
+        foreach (string module in modules)
+        {
+            configurationBuilder.AddJsonFile($"modules.{module}.json", false, true);
+            configurationBuilder.AddJsonFile($"modules.{module}.Development.json", false, true);
+        }
+    }
+}
---
+++ src/API/Evently.Ticketing.Api/OpenTelemetry/DiagnosticsConfig.cs
@@ -0,0 +1,6 @@
+﻿namespace Evently.Ticketing.Api.OpenTelemetry;
+
+internal static class DiagnosticsConfig
+{
+    public const string ServiceName = "evently.ticketing.api";
+}
